a57c16fc526ba562588766041bad295bb7cf85f0
Derek Menteer <derek.menteer@hashicorp.com>
2022-10-18 11:59:37 -0500

    Temp commit to try to support partitions.

--- peering/main.tf
+++ peering/main.tf
@@ -1,7 +1,4 @@
 terraform {
-  # This is local, because it's simply generating from a template and not changing consul.
-  # The output files from this module are what should be used with terraform cloud / remote.
-  backend "local" {}
   required_providers {
     local = {
       source  = "hashicorp/local"
@@ -21,6 +18,11 @@ variable "provider_json_file" {
   default = "consul_providers.tf.json"
 }
 
+variable "partitions" {
+  type = map(list(string))
+  default = {}
+}
+
 
 resource "local_file" "output_main_file" {
   content = templatefile("${path.module}/template.tftpl", {
@@ -34,6 +36,12 @@ resource "local_file" "output_provider_file" {
   filename = "${var.write_to_dir}/consul_providers.tf.json"
 }
 
+# TODO remove this
+resource "local_file" "test_file" {
+  content = jsonencode(local.a_partition_pairs)
+  filename = "${var.write_to_dir}/test_output.json"
+}
+
 
 locals {
   consul_providers = jsondecode(file("${var.provider_json_file}")).provider.consul
@@ -45,10 +53,57 @@ locals {
     for a in local.order: [
       for d in local.order:
         index(local.order, a) < index(local.order, d)
-          ? { acceptor: a, dialer: d }
+          ? { acceptor: a, dialer: d, acceptor_partition: "", dialer_partition: "" }
           : {}
     ]
   ])
-  pairs = setsubtract(distinct(local.unclean_pairs), [{}])
+  cluster_pairs = setsubtract(distinct(local.unclean_pairs), [{}])
+
+  # We could stop here if partitions didn't exist.
+  # pairs = local.cluster_pairs
+
+
+
+  # Attach any defined partitions.
+  a_partition_pairs = flatten([
+    for p in local.cluster_pairs: [
+      for a in try(var.partitions[p.acceptor], []):
+       {
+        acceptor: p.acceptor,
+        dialer: p.dialer,
+        acceptor_partition: a,
+       }
+    ]
+  ])
+  d_partition_pairs = flatten([
+    for p in local.cluster_pairs: [
+      for d in try(var.partitions[p.dialer], []):
+       {
+        acceptor: p.acceptor,
+        dialer: p.dialer,
+        dialer_partition: d,
+       }
+    ]
+  ])
+
+  /*
+  cross_product_pairs = flatten([
+    for a in 
+  ])
+
+  # Merge the two different partition lists together
+  a_lookup_pairs = {
+    for p in local.a_partition_pairs: "${p.acceptor};${p.acceptor_partition};${p.dialer};${p.dialer_partition}" => p
+  }
+  d_lookup_pairs = {
+    for p in local.d_partition_pairs: "${p.acceptor};${p.acceptor_partition};${p.dialer};${p.dialer_partition}" => p
+  }
+  pairs = distinct([
+    for 
+  ])
+  */
+  
+
+  pairs = local.cluster_pairs
 }
 
